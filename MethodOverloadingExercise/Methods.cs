abedfad5fa46c5102b8dbe9d9cdb116a9b8da689
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-15 20:28:47 -0500

    initial commit

---
+++ MethodOverloadingExercise/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingExercise
+{
+    internal class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool myBool)
+        {            
+            if (myBool == true)
+            {
+                int sum = num1 + num2;
+                if (sum == 1)
+                {
+                    return $"{sum} dollar";
+                }
+                else return $"{sum} dollars";
+            }
+            else
+            {
+                return "Bummer, you don't get anything.";
+            }
+        }
+    }
+}
---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,7 @@
+﻿using MethodOverloadingExercise;
+
+Console.WriteLine(Methods.Add(2, 3));
+Console.WriteLine(Methods.Add(2.22m, 3.33m));
+Console.WriteLine(Methods.Add(2, 3, true));
+Console.WriteLine(Methods.Add(0, 1, true));
+Console.WriteLine(Methods.Add(2, 3, false));
